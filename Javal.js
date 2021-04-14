7cfdfdb15e0d949412bb676b64d2a55a8776899b
Isak-Web <70743274+Isak-Web@users.noreply.github.com>
2021-04-14 16:39:08 +0200

    Add files via upload

--- Javal.js
+++ Javal.js
@@ -1,4 +1,19 @@
-function button()
-"use strict";globalThis.__codeBoxId = "bg1hjudmfy";
+let Name     = document.getElementbyId('.form-name');
+let Mail     = document.getElementbyId('.form-mail');
+let Password = document.getElementbyId('.form-password');
 
-localStorage.setItem('test2', 2);
+
+
+
+
+
+function button(){
+    localStorage.setItem('Name', 1);
+    localStorage.setItem('Mail',2);
+    localStorage.setItem('Password',3);
+
+}
+
+
+    
+
