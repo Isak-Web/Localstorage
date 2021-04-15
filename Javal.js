1b6e91d4a6511f699cfbc22327d4f438825fc785
Isak-Web <70743274+Isak-Web@users.noreply.github.com>
2021-04-15 15:54:57 +0200

    Add files via upload

--- Javal.js
+++ Javal.js
@@ -1,21 +1,23 @@
-let key=0
+/*create global vairable*/
+/*creatw for loop*/
+/*json*/
 
 
-let name     = document.getElementbyId('name');
-let mail     = document.getElementbyId('mail');
+
+
+let name = document.getElementbyId('name');
+let mail = document.getElementbyId('mail');
 let password = document.getElementbyId('password');
-
-
-
 
 
 
 function local(){
     localStorage.setItem('Name', name.value);
     localStorage.setItem('Mail', mail.value);
-    localStorage.setItem('Password', password.value);
+    
 
 }
+
 
 
 
