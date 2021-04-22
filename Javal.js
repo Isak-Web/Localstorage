178e0e34bdee7e1926dd569760739be94ae530e9
Isak-Web <70743274+Isak-Web@users.noreply.github.com>
2021-04-22 08:59:30 +0200

    Add files via upload

--- Javal.js
+++ Javal.js
@@ -1,10 +1,11 @@
 /*create global vairable*/
 /*creatw for loop*/
 /*json*/
+/*getitem*/
 
 
 
-
+/*
 let name = document.getElementbyId('name');
 let mail = document.getElementbyId('mail');
 let password = document.getElementbyId('password');
@@ -18,9 +19,23 @@ function local(){
 
 }
 
+console.log(JSON.parse)
+*/
 
 
+let name = document.getElementbyId('name');
+let mail = document.getElementbyId('mail');
 
 
-    
-
+if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("mail", "Smith");
+    // Retrieve
+    document.getElementById("name").innerHTML = localStorage.getItem("mail");
+  } else if(storage){
+    localStorage.getItem("name","mail");
+  }
+  
+  else {
+    document.getElementById("name").innerHTML = "Sorry, your browser does not support Web Storage...";
+  }
